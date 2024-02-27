0c3635c8e67c74205f335951657fd85672ad4515
Emilio Castellanos <22007232@galileo.edu>
2024-02-26 21:51:22 -0600

    Ya completo y funcional

--- js.js
+++ js.js
@@ -1,14 +1,69 @@
-const strBut = document.querySelector('#strBut');
-const pageImg = document.getElementById('pageImg');
 const pokes = ['mudkip', 'swellow', 'treecko', 'torchic', 'lotad', 'aggron', 'wailord', 'cradily', 'milotic', 'absol',
                 'metagross', 'latios', 'rayquaza', 'flygon', 'torkoal', 'hariyama', 'shedinja', 'gardevoir', 'regirock', 'kyogre'];
+
+const strBut = document.querySelector('#strBut');
+const but1 = document.getElementById('option1');
+const but2 = document.getElementById('option2');
+const but3 = document.getElementById('option3');
+const but4 = document.getElementById('option4');
+const pageImg = document.getElementById('pageImg');
+
+but1.disabled = true;
+but2.disabled = true;
+but3.disabled = true;
+but4.disabled = true;
+
 var link = '';
+var pokeOptions = [];
+var pokeI = [];
+var right = 0;
+var userScore = 0;
+var isStart = 0;
+
+
 
 strBut.addEventListener('click', async() => {
-    
-    var pokeOptions = [];
-    var pokeI = [];
 
+    doChecks();
+
+    isStart++;
+
+    strBut.disabled = true;
+    but1.disabled = false;
+    but2.disabled = false;
+    but3.disabled = false;
+    but4.disabled = false;
+
+    setOptions();
+    setSprite();
+
+    console.log(isStart);
+    
+});
+
+
+
+//Some funcitonality checks
+function doChecks(){
+    if(isStart === 0){
+        strBut.textContent = 'Next Cuestion';
+        userScore = 0;
+        document.getElementById('score').textContent = `${userScore}/3`
+        document.getElementById('endScore').textContent = '';
+    }else if(isStart === 3){
+        strBut.textContent = 'Start';
+        isStart = 0;
+
+        document.getElementById('endScore').innerHTML = `Congrats! Your final score is ${userScore}/3<br>Press Start to play again!`;
+
+
+    }
+}
+
+
+
+//Set the pokemons' options in the buttons
+function setOptions(){
     console.log(pokeOptions);
     
     while(pokeOptions.length < 4){
@@ -19,17 +74,18 @@ strBut.addEventListener('click', async() => {
         }
     }
     
-    const right = Math.floor(Math.random() * (4));
+    right = Math.floor(Math.random() * (4));
     console.log(`llena: ${pokeOptions}, ${right}`);
 
     for(let j = 0; j < 4; j++){
-        console.log(`asignacion: ${j+1}`);
         document.getElementById(`option${j+1}`).textContent=pokeOptions[j];
-        console.log(j);
     }
+}
 
-    
 
+
+//Generate and set the srite of the new pokemon
+async function setSprite(){
     try{
         link = `https://pokeapi.co/api/v2/pokemon/${pokeOptions[right]}/`;
         const response = await fetch(link);
@@ -39,5 +95,79 @@ strBut.addEventListener('click', async() => {
     }catch(error){
         alert(`Error: ${error.message}`);
     }
+}
 
+
+
+//Buttons part
+but1.addEventListener('click', () => {
+    strBut.disabled = false;
+    but1.disabled = true;
+    but2.disabled = true;
+    but3.disabled = true;
+    but4.disabled = true;
+    
+    if(but1.textContent === pokeOptions[right]){
+        userScore++;
+        document.getElementById('score').textContent = `${userScore}/3`
+    }
+    
+    doChecks();
+
+    pokeOptions = [];
+    pokeI = [];
+});
+
+but2.addEventListener('click', () => {
+    strBut.disabled = false;
+    but1.disabled = true;
+    but2.disabled = true;
+    but3.disabled = true;
+    but4.disabled = true;
+    
+    if(but2.textContent === pokeOptions[right]){
+        userScore++;
+        document.getElementById('score').textContent = `${userScore}/3`
+    }
+    
+    doChecks();
+
+    pokeOptions = [];
+    pokeI = [];
+});
+
+but3.addEventListener('click', () => {
+    strBut.disabled = false;
+    but1.disabled = true;
+    but2.disabled = true;
+    but3.disabled = true;
+    but4.disabled = true;
+    
+    if(but3.textContent === pokeOptions[right]){
+        userScore++;
+        document.getElementById('score').textContent = `${userScore}/3`
+    }
+    
+    doChecks();
+
+    pokeOptions = [];
+    pokeI = [];
+});
+
+but4.addEventListener('click', () => {
+    strBut.disabled = false;
+    but1.disabled = true;
+    but2.disabled = true;
+    but3.disabled = true;
+    but4.disabled = true;
+    
+    if(but4.textContent === pokeOptions[right]){
+        userScore++;
+        document.getElementById('score').textContent = `${userScore}/3`
+    }
+    
+    doChecks();
+
+    pokeOptions = [];
+    pokeI = [];
 });
